1fa1b9428bde9eab7453497dff44cfac6381d8ec
sarazoher <sucrosefinest@gmail.com>
2025-11-28 05:14:46 +0200

    Project main structure and base template
    Set up folders and base layout.
    
    - Added folders: templates/, static/, models/, routes/, services/.
    - Created config.py and updated app.py to load configuration.
    - Added Bootstrap layout (templates/base.html)
    - Added home page template (templates/home.html)

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from config import Config
+
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return "hello"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
